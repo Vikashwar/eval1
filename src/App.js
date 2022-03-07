e23bf865682a7f60cedf54c296ff26271ac636e6
Vikashwar <93377543+Vikashwar@users.noreply.github.com>
2022-03-07 10:24:32 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import {Counter} from "./components/Counter";
+// import logo from './logo.svg';
+// import './App.css';
+// import {Counter} from "./components/Counter";
 function App() {
+  const [book,setbook]=useState(0)
+
+  const handlebook=(value)=>{
+    setbook(book +value)
+  }
   return (
-    <div className="App">
+    <div className="items">
+      <span>Books:{book}</span>
+      <button className="addBook" onClick={()=>}
      
     </div>
   );
